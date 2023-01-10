75e47e5536bb4579af2c4b21a800a8702ec104cf
Andrea Barreiro <andymarce83@gmail.com>
2023-01-10 16:39:33 -0300

    trabajando en la pagina registro

--- JavaScript/registro.js
+++ JavaScript/registro.js
@@ -3,64 +3,132 @@
 let arrPacientes = [];
 
 class Paciente {
-    constructor(nombreyApellido, dni, edad, obraSocial, telefono, email, password) {
-
-        this.nombreyApellido = nombreyApellido;
-        this.dni = dni;
-        this.edad = edad;
-        this.obraSocial = obraSocial;
-        this.telefono = telefono;
-        this.email = email;
-        this.password = password;
-    }
+  constructor(
+    nombreyApellido,
+    dni,
+    edad,
+    obraSocial,
+    telefono,
+    email,
+    password
+  ) {
+    this.nombreyApellido = nombreyApellido;
+    this.dni = dni;
+    this.edad = edad;
+    this.obraSocial = obraSocial;
+    this.telefono = telefono;
+    this.email = email;
+    this.password = password;
+  }
 }
 
-let formReg = document.getElementById("formReg");
-
-
 function registrarse() {
+  let nombreyApellido = document.getElementById("grupo__nombreyApellido").value;
+  let dni = document.getElementById("grupo__dni").value;
+  let edad = document.getElementById("grupo__edad").value;
+  let obraSocial = document.getElementById("grupo__obraSocial").value;
+  let telefono = document.getElementById("grupo__telefono").value;
+  let email = document.getElementById("grupo__email").value;
+  let password = document.getElementById("grupo__password").value;
 
-    let nombreyApellido = document.getElementById("nombreyApellido").value;
-    let dni = document.getElementById("dni").value;
-    let edad = document.getElementById("edad").value;
-    let obraSocial = document.getElementById("obraSocial").value;
-    let telefono = document.getElementById("telefono").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+  if (
+    nombreyApellido !== "" &&
+    dni !== "" &&
+    edad !== "" &&
+    obraSocial !== "" &&
+    telefono !== "" &&
+    email !== ""
+  ) {
+    Swal.fire({
+      title: "Datos ingresados correctamente",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+    //alert("su registro se realizo correctamente. Acceda desde el portal de Pacientes a su cuenta");
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "error",
+      text: "Debe ingresar todos los campos!",
+      footer: "<complete todos los datos</a>",
+    });
+    //alert("Ingrese todos los datos");
+  }
 
+  arrPacientes.push(
+    new Paciente(
+      nombreyApellido,
+      dni,
+      edad,
+      obraSocial,
+      telefono,
+      email,
+      password
+    )
+  );
+  localStorage.setItem("PacientesTotales", JSON.stringify(arrPacientes));
+  console.log(arrPacientes);
+}
 
+const formReg = document.getElementById("formReg");
+const inputs = document.querySelectorAll("#formReg input");
 
-    if ((nombreyApellido !== "") && (dni !== "") && (edad !== "") && (obraSocial !== "") && (telefono !== "") && (email !== "") && (password !== "")) {
-        alert("su registro se realizo correctamente. Acceda desde el portal de Pacientes a su cuenta");
-    } else {
-        alert("Ingrese todos los datos");
-    }
-
-    arrPacientes.push(new Paciente(nombreyApellido, dni, edad, obraSocial, telefono, email, password));
-    localStorage.setItem("PacientesTotales", JSON.stringify(arrPacientes));
-    console.log(arrPacientes);
+const expresiones = {
+  password: /^.{6,10}$/ //6 a 10 digitos
 };
 
+const campos = {
+  password: false,
+};
 
+const ValidarFormulario = (e) => {
+  switch (e.target.name) {
+    case "password":
+      if (expresiones.password.test(e.target.value)) {
+        document
+          .querySelector("grupo__password .formulario__mensaje-password")
+          .classList.remove("formulario__mensaje-password-activo");
+        campos["password"] = true;
+      } else {
+        document
+          .querySelector("#grupo__password .formulario__mensaje-password")
+          .classList.add("formulario__mensaje-password-activo");
+        campos["password"] = false;
+      }
+      break;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", ValidarFormulario);
+  input.addEventListener("blur", ValidarFormulario);
+});
 
 formReg.addEventListener("submit", (e) => {
-    e.preventDefault()
-    registrarse()
-    formReg.reset()
-})
+  e.preventDefault();
+  registrarse();
+
+  if (campos.password) {
+    formReg.reset();
+    document
+      .getElementById("formulario__mensaje-exito")
+      .classList.add("formulario__mensaje-exito-activo");
+    setTimeout(() => {
+      document
+        .getElementById("formulario__mensaje-exito")
+        .classList.remove("formulario__mensaje-exito-activo");
+    }, 4000);
+  }
+});
 
 //.......Portal Pacientes.........//
 
-
-
 //........Turnos................//
 let encabezado = document.createElement("h3");
-encabezado.innerHTML = "<p>Los dias de atencion son Lunes, Miercoles y Viernes</p>";
+encabezado.innerHTML =
+  "<p>Los dias de atencion son Lunes, Miercoles y Viernes</p>";
 document.body.append(encabezado);
-
-
-
-
-
-
-
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,48 +1,62 @@
-
 //.....Alertas Head......//
 
 let btnDireccion = document.getElementById("showDireccion");
 btnDireccion.addEventListener("click", () => {
-    Swal.fire({
-        title: 'Pediatra',
-        text: 'Almafuerte 5543, Castelar',
-        imageUrl: 'https://streetviewpixels-pa.googleapis.com/v1/thumbnail?output=thumbnail&cb_client=lu.gallery.gps&panoid=awqCqCumv4m8oUnVhHxQAg&w=141&h=101&thumb=2&yaw=342.15317&pitch=0',
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Foto Consultorio Castelar',
-    })
+  Swal.fire({
+    title: "Pediatra",
+    text: "Almafuerte 5543, Castelar",
+    imageUrl:
+      "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?output=thumbnail&cb_client=lu.gallery.gps&panoid=awqCqCumv4m8oUnVhHxQAg&w=141&h=101&thumb=2&yaw=342.15317&pitch=0",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Foto Consultorio Castelar",
+  });
 });
 
+let btnHorarios = document.getElementById("showHorarios");
+btnHorarios.addEventListener("click", ()=> {
+  Swal.fire({
+    title: 'lunes, miercoles y viernes de 15 a 20 hs.',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  });
+})
+
+let btnContacto = document.getElementById("showContacto");
+btnContacto.addEventListener("click", ()=> {
+  Swal.fire('dra.PerezA@gmail.com')
+})
 
 //...........Pedir Turno............//
 
 let diaSemana = {
-    1: "Lunes",
-    2: "Miercoles",
-    3: "Viernes",
+  1: "Lunes",
+  2: "Miercoles",
+  3: "Viernes",
 };
 
 let horario = {
-    1: "14 hs",
-    2: "15 hs",
-    3: "16 hs",
-    4: "17 hs",
-    5: "18 hs",
-}
+  1: "14 hs",
+  2: "15 hs",
+  3: "16 hs",
+  4: "17 hs",
+  5: "18 hs",
+};
 
 let limite = 3;
 
 for (let index = 1; index <= limite; index++) {
-    console.log("El dia de la semana es" + diaSemana[index]);
+  console.log("El dia de la semana es" + diaSemana[index]);
 }
 
 let limiteHor = 5;
 
 for (let index = 1; index <= limiteHor; index++) {
-    console.log("El hoario es " + horario[index]);
-
+  console.log("El hoario es " + horario[index]);
 }
 
-
-
-
+//....Mostrar fecha en el login.......//
